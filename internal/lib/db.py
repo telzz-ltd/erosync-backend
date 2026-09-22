fe054c6eb916dcb035d410bbfa62cb182db7ebde
Usman Muhammad <usmaarn@gmail.com>
2026-09-22 08:13:04 +0100

    new commit

--- internal/lib/db.py
+++ internal/lib/db.py
@@ -3,3 +3,4 @@ import os
 from psycopg_pool import ConnectionPool
 
 pool: ConnectionPool = ConnectionPool(os.getenv("DB_URL", ""), open=False)
+
--- internal/lib/email.py
+++ internal/lib/email.py
@@ -1,19 +1,28 @@
-from jinja2 import Environment, FileSystemLoader, select_autoescape
 from enum import StrEnum
 from smtplib import SMTP
 
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+
 class Template(StrEnum):
-    VERIFICATION_CODE="verification_code"
+    VERIFICATION_CODE = "verification_code"
     PASSWORD_RESET = "password_reset"
     WELCOME = "welcome"
 
 
 def load_template(template_name: str, **kwargs) -> str:
-    env = Environment(loader=FileSystemLoader("templates/email"), autoescape=select_autoescape())
+    env = Environment(
+        loader=FileSystemLoader("templates/email"), autoescape=select_autoescape()
+    )
     template = env.get_template(f"{template_name}.html")
     return template.render(kwargs)
+
 
 def send_mail(template: str, args: dict, to: str):
     with SMTP("127.0.0.1") as smtp:
         smtp.noop()
-        smtp.send_message(msg=load_template(template, **args), from_addr="support@erosyncng.com", to_addrs=to)
+        smtp.send_message(
+            msg=load_template(template, **args),
+            from_addr="support@erosyncng.com",
+            to_addrs=to,
+        )
